430fafe209186403007a4790498f7d2017187dfb
jonahshader <jonah.shader@gmail.com>
2022-11-04 00:32:52 -0400

    working on dense_pos_layer

--- dense_pos_layer.jl
+++ dense_pos_layer.jl
@@ -1,3 +1,4 @@
+using Flux
 include("funs.jl")
 
 struct DensePosLayer
@@ -6,4 +7,21 @@ struct DensePosLayer
     biases::AbstractVector
     activation::Function
     dist_scale::Function
+end
+
+function DensePosLayer(input_size::Int, output_size::Int; pos_dims = 2, act = identity, dist_scl = bellcurve)
+    weights = randn(output_size, input_size)
+    bias = zeros(output_size)
+    positions = randn(output_size, pos_dims)
+end
+Flux.@functor DensePosLayer
+Flux.trainable(a::DensePosLayer) = (a.positions, a.weights, a.biases)
+
+function (a::DensePosLayer)(x, pos)
+    return (a.weights .* mm_dist2(a.positions, pos)) * x
+end
+
+function correct_scale(a::DensePosLayer, other_pos)
+    avg_scale = sum(a.dist_scale(mm_dist2(a.positions, other_pos))) / (size(a.positions)[1] * sizes(other_pos)[1])
+    a.weights ./= avg_scale
 end
