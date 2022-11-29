367c4071640e067187cad7645d39d600d24bb304
jonahshader <jonah.shader@gmail.com>
2022-11-29 15:53:53 -0500

    added autoencoder visualizations

--- mnist_autoencoder.jl
+++ mnist_autoencoder.jl
@@ -35,29 +35,33 @@ function run(epochs=1; opt=Adam())
     train(model, dataloader, epochs=epochs, opt=opt), train_x, train_y
 end
 
-function run_cuda(epochs=1; opt=Adam())
+function run_cuda(epochs=1; opt=Adam(), save_vis=false)
     dataloader, train_x, train_y = get_data_cuda(128)
     model = make_model() |> gpu
-    train(model, dataloader, epochs=epochs, opt=opt), train_x, train_y
+    train(model, dataloader, epochs=epochs, opt=opt, save_vis=save_vis), train_x, train_y
 end
 
-function train(model, dataloader; epochs=1, opt=Adam())
+function train(model, dataloader; epochs=1, opt=Adam(), save_vis=false)
     i = 1
     penalty() = sum([sum(model[i].weight .^ 2) + sum(model[i].bias .^ 2) for i in 1:length(model)]) * 0.000002f0
     loss(x) = Flux.mse(model(x), x) + penalty()
     ps = Flux.params(model[begin:end-1], model[end].weight, model[end].bias)
     # ps = Flux.params([m.positions for m in model])
+    anim = Animation()
     for _ in 1:epochs
         for (x, y) in dataloader
             i = i + 1
             # try
                 if i % 20 == 0
                     println(loss(x))
-                    pos_cpu = [model[1].positions, model[2].positions, model[3].positions, model[4].positions] |> cpu
-                    scatter(pos_cpu[1][:, 1], pos_cpu[1][:, 2], markersize=2)
-                    scatter!(pos_cpu[2][:, 1], pos_cpu[2][:, 2], markersize=2)
-                    scatter!(pos_cpu[3][:, 1], pos_cpu[3][:, 2], markersize=2) |> display
-                    # scatter!(pos_cpu[4][:, 1], pos_cpu[4][:, 2], markersize=2)
+                    pos_cpu = [model[1].previous_positions, model[1].positions, model[2].positions, model[3].positions, model[4].positions] |> cpu
+                    scatter(pos_cpu[1][:, 1], pos_cpu[1][:, 2], markersize=1, seriesalpha=0.25)
+                    scatter!(pos_cpu[2][:, 1], pos_cpu[2][:, 2], markersize=3)
+                    scatter!(pos_cpu[3][:, 1], pos_cpu[3][:, 2], markersize=3)
+                    scatter!(pos_cpu[4][:, 1], pos_cpu[4][:, 2], markersize=3) |> display
+                    if save_vis
+                        frame(anim)
+                    end
                 end
                 grad = gradient(() -> loss(x), ps)
                 update!(opt, ps, grad)
@@ -69,6 +73,9 @@ function train(model, dataloader; epochs=1, opt=Adam())
             # end
     
         end
+    end
+    if save_vis
+        gif(anim, "AutoEncoder Animation.gif", fps=30)
     end
     return model
 end
